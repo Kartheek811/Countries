0cec8957012fefe983524585a8fc0d0f215afd99
Kartheek811 <144550238+Kartheek811@users.noreply.github.com>
2025-10-20 18:07:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,67 +1,46 @@
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
+let result = document.getElementById("result");
 
-// ✅ Attach the Enter key listener OUTSIDE the click event
 countryInp.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    searchBtn.click(); // triggers the same search logic
+    searchBtn.click();
   }
 });
 
 searchBtn.addEventListener("click", () => {
-  let countryName = countryInp.value;
-  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
-  console.log(finalURL);
+  let countryName = countryInp.value.trim();
+  if (!countryName) {
+    result.innerHTML = `<h3>The input field cannot be empty</h3>`;
+    return;
+  }
 
+  let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   fetch(finalURL)
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) throw new Error("Country not found");
+      return response.json();
+    })
     .then((data) => {
+      let capital = data[0].capital ? data[0].capital[0] : "N/A";
+      let currency = data[0].currencies
+        ? `${data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}`
+        : "N/A";
+      let languages = data[0].languages ? Object.values(data[0].languages).join(", ") : "N/A";
+
       result.innerHTML = `
         <img src="${data[0].flags.svg}" class="flag-img">
         <h2>${data[0].name.common}</h2>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Capital:</h4>
-                <span>${data[0].capital[0]}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Continent:</h4>
-                <span>${data[0].continents[0]}</span>
-            </div>
-        </div>
-         <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Population:</h4>
-                <span>${data[0].population}</span>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Currency:</h4>
-                <span>${
-                  data[0].currencies[Object.keys(data[0].currencies)].name
-                } - ${Object.keys(data[0].currencies)[0]}</span>
-            </div>
-        </div>
-         <div class="wrapper">
-            <div class="data-wrapper">
-                <h4>Common Languages:</h4>
-                <span>${Object.values(data[0].languages)
-                  .toString()
-                  .split(",")
-                  .join(", ")}</span>
-            </div>
-        </div>
+        <div class="wrapper"><div class="data-wrapper"><h4>Capital:</h4><span>${capital}</span></div></div>
+        <div class="wrapper"><div class="data-wrapper"><h4>Continent:</h4><span>${data[0].continents[0]}</span></div></div>
+        <div class="wrapper"><div class="data-wrapper"><h4>Population:</h4><span>${data[0].population}</span></div></div>
+        <div class="wrapper"><div class="data-wrapper"><h4>Currency:</h4><span>${currency}</span></div></div>
+        <div class="wrapper"><div class="data-wrapper"><h4>Common Languages:</h4><span>${languages}</span></div></div>
       `;
     })
     .catch(() => {
-      if (countryName.length == 0) {
-        result.innerHTML = `<h3>The input field cannot be empty</h3>`;
-      } else {
-        result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
-      }
+      result.innerHTML = `<h3>Please enter a valid country name.</h3>`;
     });
 });
+
